d7403e47e508cad2e0e919bbcbffad7ceb71b85a
tianyuanfang <1784634905@qq.com>
2019-03-12 15:40:55 +0800

    sprite

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    sprite: {
+      all: {
+        src: './images/*.png',
+        dest: './images/all.png',
+        destCss: './sprites.css'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-spritesmith');
+
+  grunt.registerTask('default', ['sprite']);
+};
